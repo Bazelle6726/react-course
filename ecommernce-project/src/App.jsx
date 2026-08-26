6fc7bd1d79eb4d436b936c4f3725fab618ca12cd
bazelle6726 <michael.lucas.mallia@gmail.com>
2026-08-26 09:52:36 +0200

    moved orders to react

--- ecommernce-project/src/App.jsx
+++ ecommernce-project/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './Pages/HomePage'
 import { CheckoutPage } from './Pages/CheckoutPage';
+import { OrdersPage } from './Pages/OrdersPage';
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} /> 
     </Routes>
     
   )
--- ecommernce-project/src/Pages/CheckoutPage.jsx
+++ ecommernce-project/src/Pages/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import './checkout-header.css'
 import './checkoutPage.css'
+
 export function CheckoutPage() {
         return (
             <>
