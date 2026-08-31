d16b6b9392cf63cb981f5a4583811998b41c270b
bazelle6726 <michael.lucas.mallia@gmail.com>
2026-08-31 11:51:04 +0200

    test adding to cart button

--- ecommernce-project/src/Pages/home/Product.jsx
+++ ecommernce-project/src/Pages/home/Product.jsx
@@ -21,6 +21,7 @@ export function Product({ product, loadCart }) {
         <div className="product-container">
             <div className="product-image-container">
                 <img className="product-image"
+                data-testid="product-image"
                     src={product.image} />
             </div>
 
@@ -30,7 +31,8 @@ export function Product({ product, loadCart }) {
 
             <div className="product-rating-container">
                 <img className="product-rating-stars"
-                    src={'images/ratings/rating-${product.rating.stars * 10}.png'} />
+                data-testid="product-rating-stars-image"
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                 <div className="product-rating-count link-primary">
                     {product.rating.count}
                 </div>
@@ -63,6 +65,7 @@ export function Product({ product, loadCart }) {
             </div>
 
             <button className="add-to-cart-button button-primary"
+            data-testid="add-to-cart-button"
                 onClick={addToCart}>
                 Add to Cart
             </button>
